f8e4be4bc0dd4b9c480886f3e8276aa2310473e6
Sultonov557 <sultonovzerifboy@gmail.com>
2023-08-19 11:11:56 +0500

    ateribute

--- database.js
+++ database.js
@@ -3,16 +3,21 @@ const mysql = require("mysql2/promise");
 let con;
 
 module.exports["connect"] = async () => {
-    con = await mysql.createConnection({
-        host: process.env.DATABASE_URL,
-        user: process.env.DATABASE_USER,
-        port: process.env.DATABASE_PORT,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-    });
+	con = await mysql.createConnection({
+		host: process.env.DATABASE_URL,
+		user: process.env.DATABASE_USER,
+		port: process.env.DATABASE_PORT,
+		password: process.env.DATABASE_PASSWORD,
+		database: process.env.DATABASE_NAME,
+	});
 };
 
-module.exports["query"] = async (query) => {
-    const data = await con.query(query);
-    return data[0][0];
+module.exports["query"] = async (query, values) => {
+	let data;
+	if (values == undefined) {
+		data = await con.query(query);
+	} else {
+		data = await con.query(query, values);
+	}
+	return data[0][0];
 };
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,57 +5,64 @@ const authGuard = require("../middleware/auth-guard");
 const roleGuard = require("../middleware/role-guard");
 
 categoryRoute.post(
-    "/",
-    authGuard,
-    roleGuard("admin", "modernator"),
-    async (req, res) => {
-        try {
-            const { nameUZ, nameRU, description, } = req.body;
-            await db.query(
-                `INSERT category(nameUZ,nameRU,description) VALUES('${nameUZ}', '${nameRU}','${description}')`
-            );
-            res.send("done");
-        } catch (e) {
-            res.send(e.message);
-        }
-    }
+	"/",
+	authGuard,
+	roleGuard("admin", "modernator"),
+	async (req, res) => {
+		try {
+			const {
+				nameUZ,
+				nameRU,
+				description,
+				atteribute,
+				atteributeValues,
+			} = req.body;
+			await db.query(
+				"INSERT category(nameUZ,nameRU,description) VALUES(?, ?, ?)",
+				[nameUZ, nameRU, description]
+			);
+			res.send("done");
+		} catch (e) {
+			res.send(e.message);
+		}
+	}
 );
 
 categoryRoute.get("/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await db.query(`SELECT * FROM category WHERE ID=${id}`);
-        res.send(data);
-    } catch (e) {
-        res.send(e.message);
-    }
+	try {
+		const id = req.params.id;
+		const data = await db.query(`SELECT * FROM category WHERE ID=${id}`);
+		res.send(data);
+	} catch (e) {
+		res.send(e.message);
+	}
 });
 
 categoryRoute.put(
-    "/",
-    authGuard,
-    roleGuard("admin", "moderator"),
-    async (req, res) => {
-        try {
-            const { id, column, value } = req.body;
-            await db.query(
-                `UPDATE category SET ${column} = '${value}' WHERE ID= ${id}`
-            );
-            res.send("done");
-        } catch (e) {
-            res.send(e.message);
-        }
-    }
+	"/",
+	authGuard,
+	roleGuard("admin", "moderator"),
+	async (req, res) => {
+		try {
+			const { id, column, value } = req.body;
+			await db.query(
+				`UPDATE category SET ${column} = '${value}' WHERE ID= ${id}`
+			);
+			res.send("done");
+		} catch (e) {
+			res.send(e.message);
+		}
+	}
 );
 
 categoryRoute.delete("/", authGuard, roleGuard("admin"), async (req, res) => {
-    try {
-        const { id } = req.body;
-        await db.query(`DELETE FROM category WHERE ID='${id}';`);
-        res.send("done");
-    } catch (e) {
-        res.send(e.message);
-    }
+	try {
+		const { id } = req.body;
+		await db.query(`DELETE FROM category WHERE ID='${id}';`);
+		res.send("done");
+	} catch (e) {
+		res.send(e.message);
+	}
 });
 
 module.exports = categoryRoute;
